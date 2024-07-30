04ae453ff7fbac950dff5eaa6aa56ef34931da7b
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2024-07-30 18:49:54 +0200

    Replace require with import

--- src/problem5/app.ts
+++ src/problem5/app.ts
@@ -3,23 +3,22 @@
 // Then create controller and a service functions 
 // Also i need a database, i will use MongoDB free tier cluster
 
-import mongoose from "mongoose"
-import { env } from "process"
-
 // For initializing TS Express app i used this guide: https://blog.logrocket.com/how-to-set-up-node-typescript-express/
 
-const express = require('express')
-const dotenv = require('dotenv')
+import mongoose from "mongoose"
+import express from "express"
+import dotenv from "dotenv"
 
 dotenv.config()
 
 const app = express()
+
 const port = process.env.PORT || 3000
 
 const bootstrap = async () => {
-    //@ts-ignore
-    await mongoose.connect(process.env.MONGO_URI)
-    
+    process.env.MONGO_URI && await mongoose.connect(process.env.MONGO_URI)
+
+
     app.listen(port, () => {
         console.log(`Server is running on port ${port}`)
     })
