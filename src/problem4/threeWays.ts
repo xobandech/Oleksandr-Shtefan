2cf5e113fa3d1182231ddee1c417007d29e73cfc
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2024-07-30 19:20:19 +0200

    Implement Sum to n C

--- src/problem4/threeWays.ts
+++ src/problem4/threeWays.ts
@@ -1,3 +1,4 @@
+// Time Spent: ~20min
 // This sum to n have a time O(n) time complexity and have a medium efficiency
 function sum_to_n_a(n: number) {
     let sum = 0
@@ -12,9 +13,12 @@ function sum_to_n_b(n: number) {
     return n * (n + 1) / 2
 }
 
-// later
+// This function is almost same as first one just i can't imagine this third way... 
 function sum_to_n_c(n: number) {
+    const arr = Array.from({ length: n }, (_, i) => i + 1)
+    return arr.reduce((a, b) => a + b)
 }
 
 console.log('A -', sum_to_n_a(5)) // 15
 console.log('B -', sum_to_n_b(5)) // 15
+console.log('C -', sum_to_n_c(5)) // 15
