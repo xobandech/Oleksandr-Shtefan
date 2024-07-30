e724be9da6ff4536ec0e357bb98354ba294f5565
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2024-07-30 18:24:47 +0200

    Sum to n A

---
+++ src/problem4/threeWays.ts
@@ -0,0 +1,17 @@
+function sum_to_n_a(n: number) {
+    let sum = 0
+    for (let i = 1; i < n+1; i++) {
+        sum += i
+    }
+    return sum
+}
+
+function sum_to_n_b(n: number) {
+
+}
+
+function sum_to_n_c(n: number) {
+
+}
+
+console.log('A -', sum_to_n_a(5)) // 15
