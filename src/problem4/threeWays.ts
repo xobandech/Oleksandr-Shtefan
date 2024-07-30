a1776cdc0d4b293ce4332df6ee11ac6a189e69bb
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2024-07-30 18:32:20 +0200

    Comment about complexity

--- src/problem4/threeWays.ts
+++ src/problem4/threeWays.ts
@@ -1,3 +1,4 @@
+// This sum to n have a time O(n) time complexity and have a medium efficiency
 function sum_to_n_a(n: number) {
     let sum = 0
     for (let i = 1; i < n+1; i++) {
@@ -6,12 +7,14 @@ function sum_to_n_a(n: number) {
     return sum
 }
 
+// This sum to n have a time O(1) time complexity and i think have a best efficiency
 function sum_to_n_b(n: number) {
     return n * (n + 1) / 2
 }
 
+// later
 function sum_to_n_c(n: number) {
 }
 
-console.log('A -', sum_to_n_a(0)) // 15
-console.log('B -', sum_to_n_b(0)) // 15
+console.log('A -', sum_to_n_a(5)) // 15
+console.log('B -', sum_to_n_b(5)) // 15
